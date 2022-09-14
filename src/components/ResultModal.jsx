a46e82a73a5b2cb065da059aa4ab59b6cad96fbc
erol1098 <erol1098@gmail.com>
2022-09-14 17:43:05 +0300

    Results Modal Updated

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,9 +1,13 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import AppContext from '../context/app-context';
 import useFirebase from '../hooks/useFirebase';
 
+import styles from './ResultsModal.module.scss';
+
 const ResultModal = () => {
+  const navigate = useNavigate();
   const { userInfo, foodChoice } = useContext(AppContext);
   const { addNewEntry } = useFirebase();
 
@@ -13,27 +17,35 @@ const ResultModal = () => {
       'personal-info': userInfo,
       'food-choice': foodChoice,
     });
+    navigate('/');
   };
 
   return (
-    <div>
-      <p>Do you confirm these info?</p>
-      <div>
-        <p>First Name: {userInfo?.firstName} </p>
-        <p>Last Name: {userInfo?.lastName} </p>
-        <p>Birth Date : {userInfo?.birth} </p>
-        <p>Gender: {userInfo?.gender} </p>
+    <>
+      <div className={styles.darkBG}></div>
+      <div className={styles.centered}>
+        <div className={styles.modal}>
+          <h3>Do you confirm these info?</h3>
+          <div className={styles.content}>
+            <p>First Name: {userInfo?.firstName} </p>
+            <p>Last Name: {userInfo?.lastName} </p>
+            <p>Birth Date : {userInfo?.birth} </p>
+            <p>Gender: {userInfo?.gender} </p>
+          </div>
+          <div className={styles.content}>
+            <p>Ingredients : {foodChoice?.ingredients} </p>
+            <p>Drink: {foodChoice?.drink} </p>
+            <p>Additional Requests : {foodChoice?.additional} </p>
+          </div>
+          <form className={styles.form} onSubmit={handleSaveReservation}>
+            <button type='button' onClick={() => navigate('/')}>
+              Cancel
+            </button>
+            <button type='submit'>Submit</button>
+          </form>
+        </div>
       </div>
-      <div>
-        <p>Ingredients : {foodChoice?.ingredients} </p>
-        <p>Drink: {foodChoice?.drink} </p>
-        <p>Additional Requests : {foodChoice?.additional} </p>
-      </div>
-      <form action='' onSubmit={handleSaveReservation}>
-        <button type='reset'>Cancel</button>
-        <button type='submit'>Submit</button>
-      </form>
-    </div>
+    </>
   );
 };
 
